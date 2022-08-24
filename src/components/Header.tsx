34a543867857183fc6e2fb530a429804b1742c37
Jose <Jose.Diago@kyndryl.com>
2022-08-24 09:55:56 +0800

    Redirect to login form after logout.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
-import { AuthContext } from "@/context/AuthContext";
-import { useContext } from "react";
-import { Link } from 'react-router-dom';
+import { AuthContext } from '@/context/AuthContext';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 type HeaderProps = {};
 
 const Header = (props: HeaderProps) => {
+  const navigate = useNavigate();
   const { user, logout } = useContext(AuthContext);
+
+  const onLogout = () => {
+    logout();
+    navigate('/login');
+  };
 
   return (
     <header className="flex justify-between px-4 py-4 bg-slate-200 items-center">
@@ -13,16 +19,14 @@ const Header = (props: HeaderProps) => {
         <Link to="/">Budgetarian</Link>
       </h1>
       <nav className="flex gap-2 text-sm">
-        {
-          !user ? (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/register">Register</Link>
-            </>
-          ) : (
-            <button onClick={logout}>Logout</button>
-          )
-        }
+        {!user ? (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        ) : (
+          <button onClick={onLogout}>Logout</button>
+        )}
       </nav>
     </header>
   );
